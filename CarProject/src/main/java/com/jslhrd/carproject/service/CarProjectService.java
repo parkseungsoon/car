9168a2777c8f40d028ffbb7ff624ad1d338f7d3d
parkseungsoon <tmdtnszz@naver.com>
2023-03-07 15:57:23 +0900

    승순/0307/java폴더 기본세팅1

---
+++ CarProject/src/main/java/com/jslhrd/carproject/service/CarProjectService.java
@@ -0,0 +1,5 @@
+package com.jslhrd.carproject.service;
+
+public interface CarProjectService {
+
+}
---
+++ CarProject/src/main/java/com/jslhrd/carproject/service/CarServiceImpl.java
@@ -0,0 +1,5 @@
+package com.jslhrd.carproject.service;
+
+public class CarServiceImpl implements CarProjectService {
+
+}
