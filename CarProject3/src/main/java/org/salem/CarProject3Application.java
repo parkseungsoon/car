6bd8f926974fcb7c8b5040de1b7369ae96f06643
admin <admin@admin-pc01>
2023-04-07 16:49:57 +0900

    류승관 프로젝트

---
+++ CarProject3/src/main/java/org/salem/CarProject3Application.java
@@ -0,0 +1,13 @@
+package org.salem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarProject3Application.class, args);
+	}
+
+}
