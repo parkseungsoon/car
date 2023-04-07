bb00406c0d225c03fd7a7df789b02436ed158854
admin <admin@admin-pc01>
2023-04-07 16:58:02 +0900

    결제시스템

---
+++ CarProject/src/main/java/org/salem/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.salem;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CarProject3Application.class);
+	}
+
+}
